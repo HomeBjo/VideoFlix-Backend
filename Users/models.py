53da90af4de25d40fb21b6fc77e103ee78d163f6
HomeBjo <lala2-wuppertal@hotmail.com>
2024-07-22 11:03:47 +0200

    delete old stuff

--- Users/models.py
+++ Users/models.py
@@ -6,17 +6,3 @@ class CustomUser(AbstractUser):
    address = models.CharField(max_length=150, default='')   
    phone = models.CharField(max_length=25, default='')
    
-groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='customuser_groups',  # Vermeidet Konflikte
-        blank=True,
-        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
-        verbose_name='groups',
-    )
-user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='customuser_permissions',  # Vermeidet Konflikte
-        blank=True,
-        help_text='Specific permissions for this user.',
-        verbose_name='user permissions',
-    )

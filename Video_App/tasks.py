af7d5ed401bdf71a266e1550e3f4ca0a60dec6dc
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-07-24 14:41:14 +0200

    +++++++++

--- Video_App/tasks.py
+++ Video_App/tasks.py
@@ -6,3 +6,5 @@ def convert_video_480p(source):
     target = file_name + '_480p.mp4'
     cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)
     run = subprocess.run(cmd, capture_output=True)
+    
+    print('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')

1699f350e8726920219f609a945c9ff9537faaa0
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-07-24 16:50:15 +0200

    added caches in settings

--- Videoflix/settings.py
+++ Videoflix/settings.py
@@ -177,3 +177,15 @@ DATABASES = {
         'PORT': config('DB_PORT', cast=int),
     }
 }
+
+#Redis
+CACHES = {    
+    "default": {        
+        "BACKEND": "django_redis.cache.RedisCache",        
+        "LOCATION": "redis://127.0.0.1:6379/1",        
+        "OPTIONS": {            
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"        
+        },        
+        "KEY_PREFIX": "Videoflix"    
+        }
+    }

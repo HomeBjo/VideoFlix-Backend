753383ece2049f490e320ecbc35652eb15614ff7
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-08-06 12:57:19 +0200

    added assign values to customUser

--- Users/serializers.py
+++ Users/serializers.py
@@ -6,12 +6,16 @@ from Video_App.serializers import VideoSerializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'password')
+        fields = '__all__'
 
     def create(self, validated_data):
+        print(validated_data)
         user = CustomUser(
             username=validated_data['username'],
-            email=validated_data['email']
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            phone=validated_data['phone'],
         )
         user.set_password(validated_data['password'])
         user.is_active = False

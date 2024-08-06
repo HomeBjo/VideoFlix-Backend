394f0ed44d0cf436be832f4459d106e7da86130b
HomeBjo <lala2-wuppertal@hotmail.com>
2024-08-06 12:12:33 +0200

    add static

--- Video_App/urls.py
+++ Video_App/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from Video_App.views import VideoViewSet, failed_job_detail
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 
@@ -14,4 +16,5 @@ urlpatterns = [
     path('set_favorites/', VideoViewSet.as_view({'post': 'set_favorites'}), name='set-favorites'),
     path('favorites/', VideoViewSet.as_view({'get': 'favorites'}), name='favorites'),
     path('failed-job/<str:job_id>/', failed_job_detail, name='failed_job_detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# http://127.0.0.1:8000/media/videos/sand/sand_master.m3u8  zugriff auf server

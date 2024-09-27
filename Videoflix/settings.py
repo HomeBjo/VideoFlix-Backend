a4443e66dcee9169b22e6e2585e32bdebb482b7b
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-09-27 20:19:18 +0200

    debug = false

--- Videoflix/settings.py
+++ Videoflix/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
